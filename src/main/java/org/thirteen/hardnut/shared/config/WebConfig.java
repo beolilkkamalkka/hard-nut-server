e444a4487ba1c619b4a17829b00439e60a1a28a2
cabbage16 <qocn16@gmail.com>
2024-07-18 07:58:31 +0900

    set: add cors mapping

--- src/main/java/org/thirteen/hardnut/shared/config/WebConfig.java
+++ src/main/java/org/thirteen/hardnut/shared/config/WebConfig.java
@@ -11,9 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:3001"
-                )
+                .allowedOrigins("*")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
