6dcf19ece518c5ffb4331b246e8a1dbed14129f9
cabbage16 <qocn16@gmail.com>
2024-07-17 14:46:32 +0900

    set: initialize

---
+++ src/test/java/org/thirteen/hardnut/HardNutApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thirteen.hardnut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardNutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
